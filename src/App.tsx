d77a9ab63051fab8f79a2fe1409275e6cb1b730c
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-02-24 19:37:29 +0300

    feat: work with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import s from "./App.module.css";
 import {Button} from "./components/Button/Button";
 import {Display} from "./components/Display";
+import {SettingsDisplay} from "./components/SettingsDisplay/SettingsDisplay";
 
 function App() {
 
@@ -15,11 +16,39 @@ function App() {
         setNum(0);
     }
 
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem("counterKey", JSON.stringify(num));
+        localStorage.setItem("counterKey1", JSON.stringify(num + 2));
+        localStorage.setItem("counterKey2", JSON.stringify(num + 4));
+    }
+
+    const getFromLocalStorageHandler = () => {
+        let valueAsString = localStorage.getItem("counterKey");
+        if (valueAsString) {
+            let valueAsNumber = JSON.parse(valueAsString);
+            setNum(valueAsNumber);
+        }
+    }
+
+    const clearLocalStorageHandler = () => {
+        localStorage.clear();
+        setNum(0);
+    }
+
     return (
         <div className={s.main}>
-            <Display result={num}/>
-            <Button name={"INC"} callBack={increment} disabled={num > 4} result={num}/>
-            <Button name={"RESET"} callBack={reset} disabled={num === 0} result={num}/>
+            <div className={s.counter}>
+                <Display result={num}/>
+                <Button name={"inc"} callBack={increment} disabled={num > 40} result={num}/>
+                <Button name={"reset"} callBack={reset} disabled={num === 0} result={num}/>
+                <Button callBack={setToLocalStorageHandler} name={"setToLocalStorage"} disabled={false}/>
+                <Button callBack={getFromLocalStorageHandler} name={"getFromLocalStorage"} disabled={false}/>
+                <Button callBack={clearLocalStorageHandler} name={"clearLocalStorage"} disabled={false}/>
+            </div>
+            {/*<div className={s.settings}>*/}
+            {/*    <SettingsDisplay/>*/}
+            {/*    <Button callBack={() => setNum} result={5} name={"set"} disabled={true}/>*/}
+            {/*</div>*/}
         </div>
     );
 }
