0863bb8d224e1af05b63dc1a916c9691de77d868
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-02-27 21:50:46 +0300

    feat: add SettingsDisplay logic

--- src/components/SettingsDisplay/SettingsDisplay.tsx
+++ src/components/SettingsDisplay/SettingsDisplay.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import s from "./SettingsDisplay.module.css"
+import {Button} from "../Button/Button";
 
-export const SettingsDisplay = () => {
+export type SettingsDisplayPropsType = {
+    callBack: (start: number, max: number) => void
+}
+
+
+export const SettingsDisplay = (props: SettingsDisplayPropsType) => {
+    const {callBack} = props;
+
+    const [startValue, setStartValue] = useState(0);
+    const [maxValue, setMaxValue] = useState(0);
+
+    const onChangeHandlerStart = (event: ChangeEvent<HTMLInputElement>) => {
+        setStartValue(Number(event.currentTarget.value));
+    }
+    const onChangeHandlerMax = (event: ChangeEvent<HTMLInputElement>) => {
+        setMaxValue(Number(event.currentTarget.value));
+    }
+
+    const setBothValues = (start: number, max: number) => {
+        callBack(start, max);
+    }
     return (
-        <div>
-
+        <div className={s.inputValues}>
+            <div>
+                Max value : <input type={"number"}
+                                   onChange={onChangeHandlerMax}/>
+            </div>
+            <div>
+                Start value : <input type={"number"}
+                                     onChange={onChangeHandlerStart}/>
+            </div>
+            <Button callBack={() => setBothValues(startValue, maxValue)} name={"set"} disabled={false}/>
         </div>
     );
 };
